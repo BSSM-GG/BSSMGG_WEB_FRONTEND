562dd63f24c07bcbb3830147b2d045d7cb92381a
KJS <digyrh456789@gmail.com>
2023-04-13 23:52:58 +0900

    UPDATE :: 랭크 페이지 메인 퍼블리싱

--- src/pages/Rank/lol/Rank.jsx
+++ src/pages/Rank/lol/Rank.jsx
@@ -1,7 +1,20 @@
+import react from 'react';
+import './Rank.scss';
 export default function Rank() {
     return (
-        <div>
+        <div className='infinite'>
+            <img src="/images/rankbg.png" className="rankbg"></img>
+            <div className='topbox'>
+                <div className='rank1'>
 
+                </div>
+                <div className='rank2'>
+
+                </div>
+                <div className='rank3'>
+
+                </div>
+            </div>
         </div>
     );
 }
