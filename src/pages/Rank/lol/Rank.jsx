686019ddf172ef7aa0e4667772c75a0e20b91ed5
KJS <digyrh456789@gmail.com>
2023-04-14 23:00:02 +0900

    UPDATE :: LOL Rank Page 완성

--- src/pages/Rank/lol/Rank.jsx
+++ src/pages/Rank/lol/Rank.jsx
@@ -1,19 +1,33 @@
 import react from 'react';
 import './Rank.scss';
+import { useInView } from 'react-intersection-observer';
+
 export default function Rank() {
+    const [ref, inView] = useInView()
     return (
         <div className='infinite'>
             <img src="/images/rankbg.png" className="rankbg"></img>
             <div className='topbox'>
                 <div className='rank1'>
-
+                    <h1 className='rankings'>Rank#2</h1>
+                    <img src='/images/yumi.png' className='rankprofile'></img>
+                    <h1 className='ranknick'>d1spel</h1>
+                    <p className='ranktier'></p>
                 </div>
                 <div className='rank2'>
-
+                    <h1 className='rankings'>Rank#1</h1>
+                    <img src='/images/yumi.png' className='rankprofile'></img>
+                    <h1 className='ranknick'>d1spel</h1>
+                    <p className='ranktier'></p>
                 </div>
                 <div className='rank3'>
-
+                    <h1 className='rankings'>Rank#3</h1>
+                    <img src='/images/yumi.png' className='rankprofile'></img>
+                    <h1 className='ranknick'>d1spel</h1>
+                    <p className='ranktier'></p>
                 </div>
+            </div>
+            <div ref={ref} className='userlist'>
             </div>
         </div>
     );
