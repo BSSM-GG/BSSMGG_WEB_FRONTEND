8606bcef2fed398cae64bacd6f5709d1c2b02d86
KJS <digyrh456789@gmail.com>
2023-04-15 23:27:12 +0900

    UPDATE :: 발로란트 랭크 업데이트

--- src/pages/Rank/val/Rank2.jsx
+++ src/pages/Rank/val/Rank2.jsx
@@ -1,8 +1,11 @@
 import './Rank2.scss';
+import React from 'react';
 export default function ValorantRank() {
     return (
-        <>
+        <div className='valin'>
+            <div className='valrankbg'>
 
-        </>
+            </div>
+        </div>
     )
 }
