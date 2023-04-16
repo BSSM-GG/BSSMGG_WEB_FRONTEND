c4f36ec0c7276dc37e9e0096970d61480e5e9450
KJS <digyrh456789@gmail.com>
2023-04-16 23:19:09 +0900

    UPDATE :: 발로란트 랭킹 유저 탑 리스트 추가

--- src/pages/Rank/val/Rank2.jsx
+++ src/pages/Rank/val/Rank2.jsx
@@ -4,7 +4,29 @@ export default function ValorantRank() {
     return (
         <div className='valin'>
             <div className='valrankbg'>
-
+                <div className='topbox'>
+                    <div className='valrank1'>
+                        <h1 className='valrankings'>Rank#2</h1>
+                        <img src='/images/yumi.png' className='valrankprofile'></img>
+                        <h1 className='valranknick'>d1spel</h1>
+                        <p className='valranktier'>Radient</p>
+                        <img className='valrankimg' src='/images/radient.webp'></img>
+                    </div>
+                    <div className='valrank2'>
+                        <h1 className='valrankings'>Rank#1</h1>
+                        <img src='/images/yumi.png' className='valrankprofile'></img>
+                        <h1 className='valranknick'>d1spel</h1>
+                        <p className='valranktier'>Radient</p>
+                        <img className='valrankimg' src='/images/radient.webp'></img>
+                    </div>
+                    <div className='valrank3'>
+                        <h1 className='valrankings'>Rank#3</h1>
+                        <img src='/images/yumi.png' className='valrankprofile'></img>
+                        <h1 className='valranknick'>d1spel</h1>
+                        <p className='valranktier'>Radient</p>
+                        <img className='valrankimg' src='/images/radient.webp'></img>
+                    </div>
+                </div>
             </div>
         </div>
     )
