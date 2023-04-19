0d86f5ba58308616178ac39decd184086a117e50
KJS <digyrh456789@gmail.com>
2023-04-19 15:42:07 +0900

    UPDATE :: 롤 랭킹 페이지 퍼블리싱 완료

--- src/pages/Build/lol/Build.jsx
+++ src/pages/Build/lol/Build.jsx
@@ -5,34 +5,45 @@ export default function Builder() {
         <div className='lolbuilder'>
             <img src='/images/vs.jpg' className='backgroundbuild'></img>
             <div className='buildfirstuser'>
-
+                <img src='/images/yumi.png' className='inpicture'></img>
+                <h1 className='username'>나는야 조용제</h1>
             </div>
             <div className='buildseconduser'>
-
+                <img src='/images/yumi.png' className='inpicture'></img>
+                <h1 className='username'>나는야 조용제</h1>
             </div>
             <div className='buildthirduser'>
-
+                <img src='/images/yumi.png' className='inpicture'></img>
+                <h1 className='username'>나는야 조용제</h1>
             </div>
             <div className='buildfourthuser'>
-
+                <img src='/images/yumi.png' className='inpicture'></img>
+                <h1 className='username'>나는야 조용제</h1>
             </div>
             <div className='buildfifthuser'>
-
+                <img src='/images/yumi.png' className='inpicture'></img>
+                <h1 className='username'>나는야 조용제</h1>
             </div>
             <div className='buildsixthuser'>
-
+                <img src='/images/yumi.png' className='inpicture'></img>
+                <h1 className='username'>나는야 조용제</h1>
             </div>
             <div className='buildsevenuser'>
+                <img src='/images/yumi.png' className='inpicture'></img>
+                <h1 className='username'>나는야 조용제</h1>
 
             </div>
             <div className='buildeightuser'>
-
+                <img src='/images/yumi.png' className='inpicture'></img>
+                <h1 className='username'>나는야 조용제</h1>
             </div>
             <div className='buildnineuser'>
-
+                <img src='/images/yumi.png' className='inpicture'></img>
+                <h1 className='username'>나는야 조용제</h1>
             </div>
             <div className='buildtenuser'>
-
+                <img src='/images/yumi.png' className='inpicture'></img>
+                <h1 className='username'>나는야 조용제</h1>
             </div>
         </div>
     );
