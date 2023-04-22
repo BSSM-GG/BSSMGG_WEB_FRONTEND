c607e39abf1454fe1be300607ebe68baf6fdee62
KJS <digyrh456789@gmail.com>
2023-04-22 23:09:34 +0900

    UPDATE :: 롤 내전 팀빌더 화면 퍼블리싱 최종 완료

--- src/pages/Build/lol/Build.jsx
+++ src/pages/Build/lol/Build.jsx
@@ -6,28 +6,52 @@ export default function Builder() {
             <img src='/images/vs.jpg' className='backgroundbuild'></img>
             <div className='buildfirstuser'>
                 <div className='teamcolor'></div>
-                <div className='bluebg'></div>
-                <div className='redbg'></div>
+                <div className='bluebg'>
+                    <p className='one'>BLUE 1</p>
+                </div>
+                <div className='redbg'>
+                    <p className='redone'>RED 1</p>
+                </div>
             </div>
+            <div className='middleline'></div>
             <div className='buildseconduser'>
                 <div className='teamcolor'></div>
-                <div className='bluebg'></div>
-                <div className='redbg'></div>
+                <div className='bluebg'>
+                    <p className='two'>BLUE 2</p>
+                </div>
+                <div className='redbg'>
+                    <p className='redtwo'>RED 2</p>
+                </div>
             </div>
+            <div className='middlelinetwo'></div>
             <div className='buildthirduser'>
                 <div className='teamcolor'></div>
-                <div className='bluebg'></div>
-                <div className='redbg'></div>
+                <div className='bluebg'>
+                    <p className='three'>BLUE 3</p>
+                </div>
+                <div className='redbg'>
+                    <p className='redthree'>RED 3</p>
+                </div>
             </div>
+            <div className='middlelinethree'></div>
             <div className='buildfourthuser'>
                 <div className='teamcolor'></div>
-                <div className='bluebg'></div>
-                <div className='redbg'></div>
+                <div className='bluebg'>
+                    <p className='four'>BLUE 4</p>
+                </div>
+                <div className='redbg'>
+                    <p className='redfour'>RED 4</p>
+                </div>
             </div>
+            <div className='middlelinefourth'></div>
             <div className='buildfifthuser'>
                 <div className='teamcolor'></div>
-                <div className='bluebg'></div>
-                <div className='redbg'></div>
+                <div className='bluebg'>
+                    <p className='five'>BLUE 5</p>
+                </div>
+                <div className='redbg'>
+                    <p className='redfive'>RED 5</p>
+                </div>
             </div>
         </div>
     );
--- src/pages/Build/val/Build2.jsx
+++ src/pages/Build/val/Build2.jsx
@@ -1,7 +1,33 @@
+import './Build2.scss'
 export default function ValorantBuilder() {
     return (
-        <>
-
-        </>
-    )
+        <div className='lolbuilder'>
+            <img src='/images/vs.jpg' className='backgroundbuild' alt=''></img>
+            <div className='buildfirstuser'>
+                <div className='teamcolor'></div>
+                <div className='bluebg'></div>
+                <div className='redbg'></div>
+            </div>
+            <div className='buildseconduser'>
+                <div className='teamcolor'></div>
+                <div className='bluebg'></div>
+                <div className='redbg'></div>
+            </div>
+            <div className='buildthirduser'>
+                <div className='teamcolor'></div>
+                <div className='bluebg'></div>
+                <div className='redbg'></div>
+            </div>
+            <div className='buildfourthuser'>
+                <div className='teamcolor'></div>
+                <div className='bluebg'></div>
+                <div className='redbg'></div>
+            </div>
+            <div className='buildfifthuser'>
+                <div className='teamcolor'></div>
+                <div className='bluebg'></div>
+                <div className='redbg'></div>
+            </div>
+        </div>
+    );
 }
