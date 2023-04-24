365d7e916580226e9600d01e1f7e8216b55b64d0
KJS <digyrh456789@gmail.com>
2023-04-24 23:59:45 +0900

    UPDATE :: 발로란트 메인 페이지 리뉴얼

--- src/pages/Main2/Maintwo.jsx
+++ src/pages/Main2/Maintwo.jsx
@@ -10,6 +10,33 @@ export default function Valorant() {
                     <p className="logotexts">Valorant</p>
                     <input type='text' className='wows' placeholder="플레이어#태그 로 검색"></input>
                     <h1 className='besting'>베스트 플레이어</h1>
+                    <div className='valuserbox'>
+                        <div className='valfirstuser'>
+                            <img src="/images/radient.webp" className='tier_diamond' alt=''></img>
+                            <img src="/images/yumi.png" className='firstimage' alt=''></img>
+                            <h2>나는 조용제</h2>
+                        </div>
+                        <div className='valseconduser'>
+                            <img src="/images/radient.webp" className='tier_grand' alt=''></img>
+                            <img src="/images/yumi.png" className='secondimage' alt=''></img>
+                            <h2>d1spel</h2>
+                        </div>
+                        <div className='valthirduser'>
+                            <img src="/images/radient.webp" className='tier_challenger' alt=''></img>
+                            <img src="/images/profile.png" className='thirdimage' alt=''></img>
+                            <h2>뱃살은 애굣살</h2>
+                        </div>
+                        <div className='valfourthuser'>
+                            <img src="/images/radient.webp" className='tier_master' alt=''></img>
+                            <img src="/images/yumi.png" className='fourthimage' alt=''></img>
+                            <h2>나는 조용제</h2>
+                        </div>
+                        <div className='valfifthuser'>
+                            <img src="/images/radient.webp" className='tier_platinum' alt=''></img>
+                            <img src="/images/yumi.png" className='fifthimage' alt=''></img>
+                            <h2>나는 조용제</h2>
+                        </div>
+                    </div>
                 </div>
             </>
         </>
