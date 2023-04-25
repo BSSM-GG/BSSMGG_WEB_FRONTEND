964f080b58c9998e9828fc89dd9856ebb0fe240a
KJS <digyrh456789@gmail.com>
2023-04-25 20:02:21 +0900

    UPDATE :: 팀빌더 디자인 교체 / 유저 리스트, 유저 기입창 퍼블리싱

--- src/pages/Build/lol/Build.jsx
+++ src/pages/Build/lol/Build.jsx
@@ -1,57 +1,112 @@
-import react from 'react';
+import React, { useEffect } from 'react';
 import './Build.scss'
 export default function Builder() {
+    const [count, setCount] = React.useState(0);
+    let reset = 0;
+    const bg = [
+        { backsrc: '/images/lolbuilderbg/Katarinas.jpg' },
+        { backsrc: '/images/lolbuilderbg/Irellia.jpg' },
+        { backsrc: '/images/lolbuilderbg/yayo.jpg' },
+        { backsrc: '/images/lolbuilderbg/lux.jpg' },
+        { backsrc: '/images/lolbuilderbg/kindred.jpg' },
+        { backsrc: '/images/lolbuilderbg/Iz.jpg' },
+        { backsrc: '/images/lolbuilderbg/ahri.png' }
+
+    ]
+    useEffect(() => {
+        if (bg.length - 1 > count) {
+            let num = count + 1;
+            setTimeout(() => { setCount(num) }, 4000);
+            console.log(bg[count].backsrc)
+        }
+        else {
+            setTimeout(() => { setCount(reset) }, 4000);
+            console.log(bg[count].backsrc)
+        }
+    }, [count]);
     return (
         <div className='lolbuilder'>
-            <img src='/images/vs.jpg' className='backgroundbuild'></img>
-            <div className='buildfirstuser'>
-                <div className='teamcolor'></div>
-                <div className='bluebg'>
-                    <p className='one'>BLUE 1</p>
+            <img src={bg[count].backsrc} className='backgroundbuild'></img>
+            <div className='blueteam'>
+                <div className='bluefirst'>
+                    <div className='usericon'>
+                    </div>
+                    <p className='username'>닉네임 1</p>
                 </div>
-                <div className='redbg'>
-                    <p className='redone'>RED 1</p>
+                <div className='bluesecond'>
+                    <div className='usericon'>
+
+                    </div>
+                    <p className='username'>닉네임 2</p>
                 </div>
-            </div>
-            <div className='middleline'></div>
-            <div className='buildseconduser'>
-                <div className='teamcolor'></div>
-                <div className='bluebg'>
-                    <p className='two'>BLUE 2</p>
+                <div className='bluethird'>
+                    <div className='usericon'>
+
+                    </div>
+                    <p className='username'>닉네임 3</p>
                 </div>
-                <div className='redbg'>
-                    <p className='redtwo'>RED 2</p>
+                <div className='bluefourth'>
+                    <div className='usericon'>
+
+                    </div>
+                    <p className='username'>닉네임 4</p>
                 </div>
-            </div>
-            <div className='middlelinetwo'></div>
-            <div className='buildthirduser'>
-                <div className='teamcolor'></div>
-                <div className='bluebg'>
-                    <p className='three'>BLUE 3</p>
-                </div>
-                <div className='redbg'>
-                    <p className='redthree'>RED 3</p>
-                </div>
-            </div>
-            <div className='middlelinethree'></div>
-            <div className='buildfourthuser'>
-                <div className='teamcolor'></div>
-                <div className='bluebg'>
-                    <p className='four'>BLUE 4</p>
-                </div>
-                <div className='redbg'>
-                    <p className='redfour'>RED 4</p>
+                <div className='bluefifth'>
+                    <div className='usericon'>
+
+                    </div>
+                    <p className='username'>닉네임 5</p>
                 </div>
             </div>
-            <div className='middlelinefourth'></div>
-            <div className='buildfifthuser'>
-                <div className='teamcolor'></div>
-                <div className='bluebg'>
-                    <p className='five'>BLUE 5</p>
+            <div className='redteam'>
+                <div className='redfirst'>
+                    <div className='usericon'>
+
+                    </div>
+                    <p className='username'>닉네임 6</p>
                 </div>
-                <div className='redbg'>
-                    <p className='redfive'>RED 5</p>
+                <div className='redsecond'>
+                    <div className='usericon'>
+
+                    </div>
+                    <p className='username'>닉네임 7</p>
                 </div>
+                <div className='redthird'>
+                    <div className='usericon'>
+
+                    </div>
+                    <p className='username'>닉네임 8</p>
+                </div>
+                <div className='redfourth'>
+                    <div className='usericon'>
+
+                    </div>
+                    <p className='username'>닉네임 9</p>
+                </div>
+                <div className='redfifth'>
+                    <div className='usericon'>
+
+                    </div>
+                    <p className='username'>닉네임 10</p>
+                </div>
+            </div>
+            <div className='buildlist'>
+                <div className='startbutton'>
+                    <img src='/images/lolbuilderbg/Build.png' className='btimg'></img>
+                </div>
+                <div className='shufflebutton'>
+                    <img src='/images/lolbuilderbg/shuffle.png' className='btimg'></img>
+                </div>
+                <input type='text' className='firstusername' placeholder='내전에 참가할 플레이어 닉네임 입력'></input>
+                <input type='text' className='secondusername' placeholder='내전에 참가할 플레이어 닉네임 입력'></input>
+                <input type='text' className='thirdusername' placeholder='내전에 참가할 플레이어 닉네임 입력'></input>
+                <input type='text' className='fourthusername' placeholder='내전에 참가할 플레이어 닉네임 입력'></input>
+                <input type='text' className='fiveusername' placeholder='내전에 참가할 플레이어 닉네임 입력'></input>
+                <input type='text' className='sixusername' placeholder='내전에 참가할 플레이어 닉네임 입력'></input>
+                <input type='text' className='sevenusername' placeholder='내전에 참가할 플레이어 닉네임 입력'></input>
+                <input type='text' className='eightusername' placeholder='내전에 참가할 플레이어 닉네임 입력'></input>
+                <input type='text' className='nineusername' placeholder='내전에 참가할 플레이어 닉네임 입력'></input>
+                <input type='text' className='tenusername' placeholder='내전에 참가할 플레이어 닉네임 입력'></input>
             </div>
         </div>
     );
